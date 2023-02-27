7961f02e6592910b1dfe2de4602bcaf1eb29c1d0
okay-matthew <mtrager@macalester.edu>
2023-02-27 12:51:13 -0600

    Create MockitoTests.java

---
+++ test/comp128/gestureRecognizer/MockitoTests.java
@@ -0,0 +1,17 @@
+package comp128.gestureRecognizer;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+
+public class MockitoTests {
+
+    @Test
+    void onMouseDown() {
+
+    }
+}
